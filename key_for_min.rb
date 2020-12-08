b9bf8e77096f2727ebffe92b3ab7f3c689d4ab83
Chip Lempke <clempke90@gmail.com>
2020-12-08 00:46:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,11 @@
 def key_for_min_value(hash)
-  lowest_key = nil
-  lowest_value = Float::INFINITY
+  lowest_name = nil
+  lowest_number = Float::INFINITY
   hash.each do |k, v|
-    if v < lowest_value
-      lowest_value = v
-      lowest_key = k
+    if v < lowest_number
+      lowest_number = v
+      lowest_name = k
     end
   end
-  lowest_key
+  lowest_name
 end
