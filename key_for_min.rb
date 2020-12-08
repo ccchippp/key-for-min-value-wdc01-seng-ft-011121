896bb8e3f67f6b4fa386c7b2d8d503c0f687858d
Chip Lempke <clempke90@gmail.com>
2020-12-08 00:28:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+lowest = []
+count = 0
     if name_hash == {}
          return nil
     else
          name_hash = name_hash.sort_by {|k, v| v}
-         name_hash[0][0]
+         lowest = name_hash[0][0]
+         if name_hash[0][count] < lowest
+           lowest = name_hash[0][count]
      end
+     count += 1
 end
